3d8a822081ea06f0afcc72a3a3efc12dbdefa0e7
yosiunico <yosiunico@gmail.com>
2017-10-25 02:36:23 +0200

    first commit

---
+++ InsertarPreguntaConImagen.php
@@ -0,0 +1,19 @@
+<?php
+    require_once('config.php');
+    $link = mysqli_connect($servidor, $usuario, $pass, $bbdd);
+
+
+    $image = addslashes(file_get_contents($_FILES['inputFile']['tmp_name']));
+
+    $sql = "INSERT INTO preguntas(mail,question,correct_answer,incorrect_answer_1, incorrect_answer_2, incorrect_answer_3, complexity, topic,image, ident) VALUES ('$_POST[email]','$_POST[question]','$_POST[correctAnswer]','$_POST[incorrectAnswer1]','$_POST[incorrectAnswer2]','$_POST[incorrectAnswer3]','$_POST[complexity]','$_POST[topic]','$image','')";
+
+
+    if(!mysqli_query($link, $sql))
+    {
+        die('Error: '.mysqli_error($link));
+    }
+    echo "<h1>Insert correcto!</h1><br>";
+    echo "<p> <a href=VerPreguntasConImagen.php> Ver Preguntas </a>";
+    echo "<br>";
+
+?>
---
+++ VerPreguntasConImagen.php
@@ -0,0 +1,20 @@
+<?php
+    require_once('config.php');
+    $link = mysqli_connect($servidor, $usuario, $pass, $bbdd);
+    $preguntas = mysqli_query($link, "SELECT * FROM preguntas");
+
+    echo "<table border=1> <tr> <th>MAIL</th> <th> QUESTION </th> <th> CORRECT ANSWER </th> <th> INCORRECT ANSWER 1 </th> <th> INCORRECT ANSWER 2 </th> <th> INCORRECT ANSWER 3 </th> <th> COMPLEXITY </th> <th> TOPIC </th> <th> IMAGE </th> </tr>";
+
+    $imagen = 'imagen.png';
+
+
+
+    while ($row = mysqli_fetch_array($preguntas)) {
+
+        //echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';//codigo bueno
+        echo '<tr> <td>'. $row['mail'] . '</td><td>'. $row['question'] . '</td><td>'. $row['correct_answer'] . '</td><td>'. $row['incorrect_answer_1'] . '</td><td>'. $row['incorrect_answer_2'] . '</td><td>'. $row['incorrect_answer_3'] . '</td><td>'. $row['complexity'] . '</td><td>'. $row['topic'] . '</td><td>'. '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>' .' </td></tr>';
+    }
+    echo "</table>";
+    $preguntas->close();
+    mysqli_close($link);
+ ?>
