933ab3ef0658ed7b41ad369a8e366966900d04d4
yosiunico <yosiunico@gmail.com>
2017-10-27 22:31:16 +0200

    Add server verification and removed client verification

--- InsertarPreguntaConImagen.php
+++ InsertarPreguntaConImagen.php
@@ -2,7 +2,7 @@
     require_once('config.php');
     $link = mysqli_connect($servidor, $usuario, $pass, $bbdd);
 
-    $image = addslashes(file_get_contents($_FILES['inputFile']['tmp_name']));
+
 
     $mail = $_POST['email'];
     $question = $_POST['question'];
@@ -12,18 +12,46 @@
     $incorrectAnswer3 = $_POST['incorrectAnswer3'];
     $complexity = $_POST['complexity'];
     $topic = $_POST['topic'];
+    $isCorrect = verify($mail, $question, $correctAnswer, $incorrectAnswer1, $incorrectAnswer2, $incorrectAnswer3, $complexity, $topic);
 
 
+    if($isCorrect){
+        $image = addslashes(file_get_contents($_FILES['inputFile']['tmp_name']));
+        $sql = "INSERT INTO preguntas(mail,question,correct_answer,incorrect_answer_1, incorrect_answer_2, incorrect_answer_3, complexity, topic,image) VALUES ('$mail','$question','$correctAnswer','$incorrectAnswer1','$incorrectAnswer2','$incorrectAnswer3','$complexity','$topic','$image')";
 
-    $sql = "INSERT INTO preguntas(mail,question,correct_answer,incorrect_answer_1, incorrect_answer_2, incorrect_answer_3, complexity, topic,image) VALUES ('$mail','$question','$correctAnswer','$incorrectAnswer1','$incorrectAnswer2','$incorrectAnswer3','$complexity','$topic','$image')";
 
-
-    if(!mysqli_query($link, $sql))
-    {
-        die('Error: '.mysqli_error($link));
+        if(!mysqli_query($link, $sql))
+        {
+            die('Error: '.mysqli_error($link));
+        }
+        echo "<h1>Insert correcto!</h1><br>";
+        echo "<p> <a href=VerPreguntasConImagen.php> Ver Preguntas </a>";
+        echo "<br>";
+    }else {
+        echo "<h1> Insert incorrecto</h1>";
     }
-    echo "<h1>Insert correcto!</h1><br>";
-    echo "<p> <a href=VerPreguntasConImagen.php> Ver Preguntas </a>";
-    echo "<br>";
+
+    function verify($mail, $question, $correctAnswer, $incorrectAnswer1, $incorrectAnswer2, $incorrectAnswer3, $complexity, $topic) {
+        $isCorrect = true;
+        if(preg_match("/^[a-zA-Z]{3,}[0-9]{3}@ikasle.ehu.eu?s$/",$mail) == 0){
+            $isCorrect = false;
+        }
+        $opciones = array(
+            'options' => array(
+                'min_range' => 0,
+                'max_range' => 5
+            )
+        );
+        if( filter_var($complexity, FILTER_VALIDATE_INT, $opciones ) == false){
+            $isCorrect = false;
+        }
+        if(preg_match("/^.{10,}$/",$question) == 0){
+            $isCorrect = false;
+        }
+        if(strlen($correctAnswer) == 0 || strlen($incorrectAnswer1) == 0 || strlen($incorrectAnswer2) == 0 || strlen($incorrectAnswer3) || strlen($topic) == 0){
+            $isCorrect = false;
+        }
+        return $isCorrect;
+    }
 
 ?>
