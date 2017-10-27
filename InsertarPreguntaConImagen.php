ec84490166d6e008e82bb05d71364412e2645184
yosiunico <yosiunico@gmail.com>
2017-10-27 13:56:36 +0200

    Cambiado el orden de la sentencia SQL de insertar

--- InsertarPreguntaConImagen.php
+++ InsertarPreguntaConImagen.php
@@ -5,7 +5,7 @@
 
     $image = addslashes(file_get_contents($_FILES['inputFile']['tmp_name']));
 
-    $sql = "INSERT INTO preguntas(mail,question,correct_answer,incorrect_answer_1, incorrect_answer_2, incorrect_answer_3, complexity, topic,image, ident) VALUES ('$_POST[email]','$_POST[question]','$_POST[correctAnswer]','$_POST[incorrectAnswer1]','$_POST[incorrectAnswer2]','$_POST[incorrectAnswer3]','$_POST[complexity]','$_POST[topic]','$image','')";
+    $sql = "INSERT INTO preguntas(mail,question,correct_answer,incorrect_answer_1, incorrect_answer_2, incorrect_answer_3, complexity, topic,image) VALUES ('$_POST[email]','$_POST[question]','$_POST[correctAnswer]','$_POST[incorrectAnswer1]','$_POST[incorrectAnswer2]','$_POST[incorrectAnswer3]','$_POST[complexity]','$_POST[topic]','$image')";
 
 
     if(!mysqli_query($link, $sql))
