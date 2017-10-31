79399c7e3724b25d1a29e7fecce89c841d6979d3
yosiunico <yosiunico@gmail.com>
2017-10-31 18:34:34 +0100

    Add fields to Registrar.php

--- Registrar.php
+++ Registrar.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta name="tipo_contenido" content="text/html;" http-equiv="content-type" charset="utf-8">
-    <title>Preguntas</title>
+    <title>Registrarse</title>
     <link rel='stylesheet' type='text/css' href='estilos/style.css' />
     <link rel='stylesheet'
           type='text/css'
@@ -25,8 +25,28 @@
         <span><a href='./creditos.html'>Creditos</a></span>
     </nav>
     <section class="main" id="s1">
-        <div>
+        <div align="left">
             <!-- Mostrar formulario para registrarse -->
+            <fieldset>
+                <legend>Registro</legend>
+                <form action="./Registrar.php" method="post">
+                    <label for="input_email">Email:*</label>
+                    <input id="input_email" name="email" title="Ej: crivas004@ikasle.ehu.es" type="text" pattern="^[a-zA-Z]{3,}[0-9]{3}@ikasle.ehu.eu?s$" required><br>
+                    <label for="input_name_lastnames">Nombre y apellidos:*</label>
+                    <input id="input_name_lastnames" name="name_lastnames" type="text" pattern="[A-Za-z\s]{2,}" title="Ej: Joseba Merino Pina" required><br>
+                    <label for="input_nick">Nick:*</label>
+                    <input id="input_nick" name="nick" type="text" pattern=".+" required><br>
+                    <label for="input_password">Contraseña:*</label>
+                    <input id="input_password" name="password" type="password" pattern=".{6,}" required><br>
+                    <label for="input_repeat_password">Repite la contraseña:*</label>
+                    <input id="input_repeat_password" name="repeat_password" type="password" pattern=".{6,}" required ><br>
+                    <label for="input_image">Elegir foto de perfil:</label>
+                    <input id="input_image" name="image" type="file"><br>
+
+                    <input type="submit" value="Enviar">
+                </form>
+            </fieldset>
+
 
         </div>
     </section>
