fb73da8bdc0f084d8ae3e7e43916223843b6b561
yosiunico <yosiunico@gmail.com>
2017-11-01 13:33:35 +0100

    Registrar es más bonito

--- Registrar.php
+++ Registrar.php
@@ -28,22 +28,25 @@
         <div align="left">
             <!-- Mostrar formulario para registrarse -->
             <fieldset>
-                <legend>Registro</legend>
-                <form action="./Registrar.php" method="post">
-                    <label for="input_email">Email:*</label>
-                    <input id="input_email" name="email" title="Ej: crivas004@ikasle.ehu.es" type="text" pattern="^[a-zA-Z]{3,}[0-9]{3}@ikasle.ehu.eu?s$" required><br>
-                    <label for="input_name_lastnames">Nombre y apellidos:*</label>
-                    <input id="input_name_lastnames" name="name_lastnames" type="text" pattern="[A-Za-z\s]{2,}" title="Ej: Joseba Merino Pina" required><br>
-                    <label for="input_nick">Nick:*</label>
-                    <input id="input_nick" name="nick" type="text" pattern=".+" required><br>
-                    <label for="input_password">Contraseña:*</label>
-                    <input id="input_password" name="password" type="password" pattern=".{6,}" required><br>
-                    <label for="input_repeat_password">Repite la contraseña:*</label>
-                    <input id="input_repeat_password" name="repeat_password" type="password" pattern=".{6,}" required ><br>
-                    <label for="input_image">Elegir foto de perfil:</label>
-                    <input id="input_image" name="image" type="file"><br>
 
-                    <input type="submit" value="Enviar">
+                <legend align="center">Registro</legend>
+                <form action="./Registrar.php" method="post">
+                    <div style="padding: 20px">
+                        <label for="input_email">Email:*</label>
+                        <input id="input_email" name="email" title="Ej: crivas004@ikasle.ehu.es" type="text" pattern="^[a-zA-Z]{3,}[0-9]{3}@ikasle.ehu.eu?s$" required><br>
+                        <label for="input_name_lastnames">Nombre y apellidos:*</label>
+                        <input id="input_name_lastnames" name="name_lastnames" type="text" pattern="[A-Za-z\s]{2,}" title="Ej: Joseba Merino Pina" required><br>
+                        <label for="input_nick">Nick:*</label>
+                        <input id="input_nick" name="nick" type="text" pattern=".+" required><br>
+                        <label for="input_password">Contraseña:*</label>
+                        <input id="input_password" name="password" type="password" pattern=".{6,}" required><br>
+                        <label for="input_repeat_password">Repite la contraseña:*</label>
+                        <input id="input_repeat_password" name="repeat_password" type="password" pattern=".{6,}" required ><br>
+                        <label for="input_image">Elegir foto de perfil:</label>
+                        <input id="input_image" name="image" type="file"><br>
+
+                        <input type="submit" value="Enviar">
+                    </div>
                 </form>
             </fieldset>
 
